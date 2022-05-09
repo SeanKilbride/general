932f64d09525c6b7665a5c2b983309ff1904fe77
SeanKilbride <61568640+SeanKilbride@users.noreply.github.com>
2022-05-09 13:43:35 +0100

    Update secs_to_hms.py

--- secs_to_hms.py
+++ secs_to_hms.py
@@ -5,6 +5,7 @@ def secs_to_hms(secs):
     secs   = hours_rem % 60   # mins remainder i.e. secs
     return [hours_full, mins_full, secs]
 
+# quick test loop
 for i in [60, 5.5*60, 7.25*60, 0.5*3600, 2.25*3600, 2.25*3600+60]:
     h_m_s_list = secs_to_hms(i)
     print(i, "seconds =     ", h_m_s_list[0], ":", h_m_s_list[1], ":", h_m_s_list[2] , " hh:mm:ss" )
